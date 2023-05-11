1073e1c962ee223dcc9da3218004a6454a31756b
wkabia12 <wkabia12@gmail.com>
2023-05-11 18:43:40 +0000

    fixes

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 if __name__ == "__main__":
-    print("1 + 2 = {}".format(add_0.add(1, 2)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    print("10 + 5 = {}".format(add(10, 5)))
-    print("10 - 5 = {}".format(sub(10, 5)))
-    print("10 * 5 = {}".format(mul(10, 5)))
-    print("10 / 5 = {}".format(div(10, 5)))
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
