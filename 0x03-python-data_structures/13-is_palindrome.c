fd0eefb7d5d1d09d32d48a4aa15ccf507897d2b8
wkabia12 <wkabia12@gmail.com>
2023-05-14 20:14:27 +0000

    13, singly linked list palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ *is_palindrome - checks palindrome in singly linked list
+ *@head: list arguement
+ *Return: 1 if True else 0
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *current;
+
+	current = *head;
+	int i = 0, j = 0, k = 0;
+
+	if (current == NULL)
+	{
+		return (1);
+	}
+	while (current)
+	{
+		current = current->next;
+		i++;
+	}
+
+	int temp[i];
+	int temp2[i];
+
+	current = *head;
+
+	while (current)
+	{
+		temp[j] = current->n;
+		current = current->next;
+		j++;
+	}
+	for (; k < i; k++)
+	{
+		temp2[i - 1 - k] = temp[k];
+	}
+	k = 0;
+	for (; k < i; k++)
+	{
+		if (temp[k] != temp2[k])
+			return (0);
+	}
+	return (1);
+}
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(my_list=[[]]):
-    if len(my_list) < 1:
-        print("{}".format())
+    if not my_list:
+        print("{}".format(''))
     else:
         for lst in my_list:
             for i in range(len(lst)):
                 if i == len(lst) - 1:
-                    print("{} ".format(lst[i]))
+                    print("{:d}".format(lst[i]))
                 else:
-                    print("{}".format(lst[i]), end=" ")
+                    print("{:d}".format(lst[i]), end=" ")
