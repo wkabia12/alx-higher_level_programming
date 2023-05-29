536a50cadfec3dd72dd9b38ea3c85f468de5f404
wkabia12 <wkabia12@gmail.com>
2023-05-29 20:47:28 +0000

    7 fix

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as ve:
-        print(ve, file=sys.stderr)
+        print("Exception: {}".format(ve), file=sys.stderr)
         return False
