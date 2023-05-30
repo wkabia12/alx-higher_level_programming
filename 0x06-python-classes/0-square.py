7f2627acc487e662bce8a609bbf3aebf6b45b7ae
wkabia12 <wkabia12@gmail.com>
2023-05-30 15:57:53 +0000

    docstring fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
+'''
+    Module defines Square class
+'''
+
+
 class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+
+    Description:
+        The `Square` class is used to define a square object.
+        It does not have any attributes or methods.
+
+    Example:
+    >>> my_square = Square()
+    """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
-class Square:
+'''
+    Module defines Square class
+'''
 
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size: private attribute to store size variable
+
+    Methods:
+        __init__: initializes an object
+
+    Description:
+        The `Square` class is used to define a square object.
+        It contains the __size attribute and __init__ meathod.
+
+    Example:
+    >>> my_square = Square(3)
+    """
     def __init__(self, size):
         self.__size = size
