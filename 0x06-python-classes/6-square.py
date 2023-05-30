6c10df9a78bbf5f5a1fb146c8acc01daa902dc52
wkabia12 <wkabia12@gmail.com>
2023-05-30 20:50:29 +0000

    Classes and Objects 5, 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+'''
+    Module defines Square class
+'''
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size: private attribute to store size variable
+
+    Methods:
+        __init__: initializes object
+        area: Returns area of Square
+
+    Description:
+        The `Square` class is used to define a square object.
+        It has a private attribute __size and a method to calculate the area.
+
+    Example:
+    >>> my_square = Square()
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = 0
+        self.__position = (0, 0)
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        x, y = value
+        if not isinstance(x, int) or not isinstance(y, int) or x < 0 or y < 0:
+            raise ValueError("position must be a tuple of 2 positive integers")
+        self.__position = (x, y)
+
+    def area(self):
+        return self.__size**2
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            i = self.__size
+            while (i > 0):
+                if self.__position[1] > 0:
+                    print("" + "#" * self.__size)
+                else:
+                    print(" " * self.__position[0] + "#" * self.__size)
+                i -= 1
