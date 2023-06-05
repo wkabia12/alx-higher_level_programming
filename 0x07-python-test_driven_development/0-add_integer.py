855cc93d22e7405e2d79d35f4c1be4b1101e3e28
wkabia12 <wkabia12@gmail.com>
2023-06-05 12:18:26 +0000

    0, TDD

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+    Module defines add_integer function
+'''
+
+
+def add_integer(a, b=98):
+    """
+    function that adds 2 integers:
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(100, -2)
+    98
+    >>> add_integer(2)
+    100
+    >>> add_integer(100.3, -2)
+    98
+    >>> add_integer(4, "School")
+    Traceback (most recent call last):
+        ...
+    TypeError: b must be an integer
+    >>> add_integer(None)
+    Traceback (most recent call last):
+        ...
+    TypeError: a must be an integer
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    return a + b
