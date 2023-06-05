f4acb2aa49b5823b570c34a2fbeb232a0f573d49
wkabia12 <wkabia12@gmail.com>
2023-06-05 22:37:49 +0000

    More Classes and Objects, 0 - 9

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+    Module defines Rectangle class
+'''
+
+
+class Rectangle:
+    """
+    Represents a rectangle.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+
+    Description:
+        The `Rectangle` class is used to define a Rectangle object.
+        It does not have any attributes or methods.
+
+    Example:
+    >>> my_rect = Rectangle()
+    """
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+'''
+    Module defines Rectangle class
+'''
+
+
+class Rectangle:
+    """
+    Represents a rectangle.
+
+    Attributes:
+        height - height of recctangle
+        width - width of recctangle
+
+    Methods:
+        __init__ - initialize Rectangle objects
+        height - sets Rectangle height
+        width - sets Rectangle width
+
+    Description:
+        The `Rectangle` class is used to define a Rectangle object.
+
+    Example:
+    >>> my_rect = Rectangle()
+    """
+    def __init__(self, width=0, height=0):
+        """ initializes height and width """
+        self.__height = 0
+        self.__width = 0
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        """ retrieve height """
+        return self.__height
+
+    @property
+    def width(self):
+        """ retrieve width """
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        """ set height with new value """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        """ set width with new value """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
