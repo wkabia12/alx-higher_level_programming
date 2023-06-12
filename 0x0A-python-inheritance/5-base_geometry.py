9d5d875b91ef61c498ab04150acc12d0c9115acc
wkabia12 <wkabia12@gmail.com>
2023-06-12 22:13:54 +0000

    1st line env fix 5 - 11

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,9 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
     Module for Empty BaseGeometry class
 """
 
 
 class BaseGeometry:
-    """ BaseGeometry empty class """
+    """
+    Represents a BaseGeometry Class.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+
+    Description:
+        The `BaseGeometry` class is used to define
+        a BaseGeometry object.
+        It does not have any attributes or methods.
+
+    Example:
+    >>> obj1 = BaseGeometry()
+    """
     pass
