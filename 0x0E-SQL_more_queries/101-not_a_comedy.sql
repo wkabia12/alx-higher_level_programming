2d5028a08bfd6c09b1d299ce05b944f81aaf4df5
wkabia12 <wkabia12@gmail.com>
2023-07-19 21:05:15 +0000

    SQL More Queries 1 - 16, 100 - 103

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,8 @@
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+SELECT DISTINCT tv_shows.title FROM tv_show_genres RIGHT JOIN tv_shows 
+ON tv_show_genres.show_id = tv_shows.id LEFT OUTER JOIN tv_genres 
+ON tv_show_genres.genre_id = tv_genres.id WHERE tv_shows.title NOT IN (SELECT tv_shows.title 
+		FROM tv_show_genres INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id 
+		INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id 
+		WHERE tv_genres.name = "Comedy") 
+ORDER BY tv_shows.title;
