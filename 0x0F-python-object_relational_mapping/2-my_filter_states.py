5e39b86ddae802529a0d8a3416a45d69517a5321
wkabia12 <wkabia12@gmail.com>
2023-08-20 19:33:00 +0000

    Python ORM 0-14

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" list states from db hbtn_0e_0_usa """
+
+
+def run_cmd(args):
+    """ print rows in states table """
+    db = Db.connect(host="localhost",
+                    port=3306,
+                    user=args[0],
+                    passwd=args[1],
+                    db=args[2])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states\
+		    WHERE name LIKE BINARY '{}%'\
+		    ORDER BY id """.format(args[3]))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
+
+if __name__ == "__main__":
+    import MySQLdb as Db
+    import sys
+    run_cmd(sys.argv[1:])
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""creates model CIty, if error, delete unique on id"""
+
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class City(Base):
+    """ create city class base """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer,
+                      ForeignKey('states.id'),
+                      nullable=False)
